815dd8c77db1d028ec4015a43b9c1024143bb567
RicardoJavierFernandez <rickyjf808@gmail.com>
2019-11-29 13:01:54 -0500

    Initial commit

---
+++ Domain/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        public int Id { get; set; } // auto-implemented property
+
+        public string Name { get; set; }
+    }
+}
